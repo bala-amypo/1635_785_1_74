7a5dd682d91991bf4ec9fb12e94b0fba2f6b900c
bala-amypo <bala@amypo.in>
2025-12-20 14:31:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/FraudDetectionContoller.java
+++ src/main/java/com/example/demo/controller/FraudDetectionContoller.java
@@ -1,3 +1,15 @@
 package com.example.demo.controller;
 
-import com.example.demo.
+import com.example.demo.model.FraudCheckResult;
+import com.example.demo.service.FraudDetectionService;
+
+@RestController
+@RequestMapping("api/claims")
+public class ClaimController{
+    @Autowired ClaimService service;
+    public ClaimController(ClaimService service){
+        this.service=service;
+    }
+    @PostMapping
+    public Claim submitClaim(@RequestBody Claim claim)
+}

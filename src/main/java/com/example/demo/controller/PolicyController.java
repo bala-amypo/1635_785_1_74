f1d4cdf305ea05e4e2229c6e05ceb750806f306c
bala-amypo <bala@amypo.in>
2025-12-20 14:16:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/PolicyController.java
+++ src/main/java/com/example/demo/controller/PolicyController.java
@@ -5,7 +5,17 @@ import com.example.demo.model.Policy;
 import com.example.demo.service.PolicyService;
 
 @RestController
-@RequestMapping()
+@RequestMapping("/api/policies")
 public class PolicyController{
+    @Autowired PolicyService service;
+    public PolicyController(PolicyService service){
+        this.service=service;
+    }
+    @PostMapping
+    public Policy createPolicy(@RequestBody Policy policy){
+        return service.savePolicy(policy);
+    }
+    @GetMapping("/user/{userId}")
+    public List<Policy>
     
 }

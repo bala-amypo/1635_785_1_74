851d3503e64be7b49c269ac9c6603c2a8e307e00
bala-amypo <bala@amypo.in>
2025-12-20 15:49:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/PolicyRepository.java
+++ src/main/java/com/example/demo/repository/PolicyRepository.java
@@ -6,5 +6,7 @@ import com.example.demo.model.User;
 
 public interface PolicyRepository extends JpaRepository<Policy, Long>{
     List<Policy>findByUser(User user);
-    boolean existsByPolicyNumber(String policyNumber)
+    boolean existsByPolicyNumber(String policyNumber);
+
+    List<P
 }

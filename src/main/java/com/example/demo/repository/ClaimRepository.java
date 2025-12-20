0cfce72bcbac30a258e0e87772e143a4ea52dcf1
bala-amypo <bala@amypo.in>
2025-12-20 15:58:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/ClaimRepository.java
+++ src/main/java/com/example/demo/repository/ClaimRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
  import com.example.demo.model.Claim;
- import ja
+ import java.util.List;
 public interface ClaimRepository extends JpaRepository<Claim,Long>{
     List<Claim> findByPolicy(Long policyId)
 }
--- src/main/java/com/example/demo/repository/FraudCheckResultRepository.java
+++ src/main/java/com/example/demo/repository/FraudCheckResultRepository.java
@@ -1,5 +1,9 @@
 package com.example.demo.repository;
-
-public interface FraudCheckResultRepository{
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.FraudCheckResult;
+import com.example.demo.model.Claim;
+public interface FraudCheckResultRepository extends JpaRepository<FraudCheckResult,Long>{
+    Optional<FraudCheckResult> findByClaim(Claim claim);
     
 }

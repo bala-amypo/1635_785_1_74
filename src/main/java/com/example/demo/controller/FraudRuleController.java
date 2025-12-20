168bd914ede3220532d46f2f39e2d2f45cbd15f0
bala-amypo <bala@amypo.in>
2025-12-20 14:25:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/FraudRuleController.java
+++ src/main/java/com/example/demo/controller/FraudRuleController.java
@@ -1,6 +1,16 @@
 package com.example.demo.controller;
 import java.util.List;
-import com.example.
+import com.example.demo.model.FraudRule;
+import com.examle.demo.service.FraudRuleService;
+
+@RestController
+@RequestMapping("/api/rules")
 public class FraudRuleController{
-    
+    @Autowired FraudRuleService service;
+
+    public FraudRuleController(FraudRuleService service){
+        this.service=service;
+    }
+    @PostMapping
+    public FraudRule addRule(@R)
 }

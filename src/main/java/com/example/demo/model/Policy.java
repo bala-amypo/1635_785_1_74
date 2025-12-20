041f12a12954c86f6b44c43bc3d9b1b8b88a2402
bala-amypo <bala@amypo.in>
2025-12-20 14:01:52 +0000

    User commit

--- src/main/java/com/example/demo/model/Policy.java
+++ src/main/java/com/example/demo/model/Policy.java
@@ -1,4 +1,16 @@
 package com.example.demo.model;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GEnerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
+@Entity
+@Data
+@AllArgsConstructor
 
 public class Policy{
     

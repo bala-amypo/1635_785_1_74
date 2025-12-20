735c974419efbb884e06db3dd29b5cfeb529341e
bala-amypo <bala@amypo.in>
2025-12-20 04:49:07 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,15 @@
 packge com.example.demo.model;
+import jakarta.persistence.Entity;
+import lombok.Data;
+import lombok.AllArgsContructor;
+impo
 
+
+@Entity
+@Data
+@AllArgsContructor
+@NoArgsConsturctor
 public class User{
+    @
     
 }

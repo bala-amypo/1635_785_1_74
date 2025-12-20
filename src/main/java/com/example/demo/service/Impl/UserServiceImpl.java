69af4d4cc85cb46378c42f328e8bf95959939c5e
bala-amypo <bala@amypo.in>
2025-12-20 07:56:07 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/UserServiceImpl.java
@@ -4,7 +4,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.service.UserService;
-import com.example.demo.entity.User;
+import com.example.demo.model.User;
 import com.example.repository.UserRepository;
 
 @Service

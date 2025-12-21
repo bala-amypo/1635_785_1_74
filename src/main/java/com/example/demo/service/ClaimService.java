3f04a4267e1930b2891e2a40f90fea7dda74947c
bala-amypo <bala@amypo.in>
2025-12-21 06:43:34 +0000

    User commit

--- src/main/java/com/example/demo/service/ClaimService.java
+++ src/main/java/com/example/demo/service/ClaimService.java
@@ -2,5 +2,7 @@ package com.example.demo.service;
 package com.example.demo.model.Claim;
 import java.util.List;
 public class ClaimService{
-    Claim createClaim(Long policyId,
+    Claim createClaim(Long policyId,Claim claim);
+    Claim getClaim(Long claimId);
+    List<Claim>getAllClaims();
 }

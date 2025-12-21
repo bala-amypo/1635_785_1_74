e421986faa18bd3b29809ec2a63e42a855ad7e0e
bala-amypo <bala@amypo.in>
2025-12-21 06:31:34 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,26 +1,38 @@
 package com.example.demo.model;
+
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import lombok.Data;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Column;
 
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class User{
+@AllArgsConstructor
+public class User {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    @Column(unique=true)
-    private String email;
-    private String password;
-    private String role;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;   
+
+    @Column(nullable = false)
+    private String name;   
+
+    @Column(nullable = false, unique = true)
+    private String email; 
+
+    @Column(nullable = false)
+    private String password;  
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;  
 }

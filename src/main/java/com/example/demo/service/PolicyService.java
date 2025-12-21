ac68a02c586e3b5fe97af7529d06b36599f0d015
bala-amypo <bala@amypo.in>
2025-12-21 13:24:50 +0000

    User commit

--- src/main/java/com/example/demo/service/PolicyService.java
+++ src/main/java/com/example/demo/service/PolicyService.java
@@ -1,9 +1,14 @@
 package com.example.demo.service;
+
 import com.example.demo.model.Policy;
+
 import java.util.List;
 
-public interface UserService{
-    Policy createPolicy(Long userId,Policy policy);
-    List<Policy> getPolicyByUser(Long userId);
+public interface PolicyService {
+
+    Policy createPolicy(Long userId, Policy policy);
+
+    List<Policy> getPoliciesByUser(Long userId);
+
     Policy getPolicy(Long id);
 }

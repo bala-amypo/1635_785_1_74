651041b6df03ceaf209ace0ee8e66a528eeb8a0a
bala-amypo <bala@amypo.in>
2025-12-21 06:37:34 +0000

    User commit

--- src/main/java/com/example/demo/service/FraudDetectionService.java
+++ src/main/java/com/example/demo/service/FraudDetectionService.java
@@ -1,4 +1,5 @@
 package com.example.demo.service;
+package com.example.demo.modelFraudDetectionService;
 
 public class FraudDetectionService{
     
--- src/main/java/com/example/demo/service/FraudRuleService.java
+++ src/main/java/com/example/demo/service/FraudRuleService.java
@@ -1,5 +1,7 @@
 package com.example.demo.service;
-
+import com.example.demo.model.FraudRuleService;
+import java.util.List;
 public class FraudRuleService{
-    
+    FraudRule addRule(FraudRule rule);
+    List<FraudRule> getAllRules();
 }

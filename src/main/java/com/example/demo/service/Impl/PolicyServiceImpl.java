34a2230c641d7a5fa96f20aca888be61b2818517
bala-amypo <bala@amypo.in>
2025-12-21 06:46:34 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/PolicyServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/PolicyServiceImpl.java
@@ -1,5 +1,12 @@
 package com.example.demo.service.Impl;
-
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.service.UserService;
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+@Service
 public class PolicyServiceImpl{
-    
+    @Autowired PolicyRepository policyRepository;
 }

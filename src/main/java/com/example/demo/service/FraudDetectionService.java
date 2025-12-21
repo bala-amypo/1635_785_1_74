6c00aa545a5ab3fc6d7ec2be7fffa0e806185505
bala-amypo <bala@amypo.in>
2025-12-21 16:08:16 +0000

    User commit

--- src/main/java/com/example/demo/service/FraudDetectionService.java
+++ src/main/java/com/example/demo/service/FraudDetectionService.java
@@ -1,8 +1,12 @@
 package com.example.demo.service;
 
+import java.util.List;
 import com.example.demo.model.Claim;
 import com.example.demo.model.FraudCheckResult;
 
 public interface FraudDetectionService {
-    FraudCheckResult checkClaim(Claim claim);
+
+    FraudCheckResult checkFraud(Claim claim);
+
+    List<FraudCheckResult> getAllResults();
 }

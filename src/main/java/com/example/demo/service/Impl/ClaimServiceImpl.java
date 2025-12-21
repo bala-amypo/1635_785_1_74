e863cf9e416dcf6c7d77ed8396195ae3d0fba660
bala-amypo <bala@amypo.in>
2025-12-21 06:55:35 +0000

    User commit

--- src/main/java/com/example/demo/service/Impl/ClaimServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/ClaimServiceImpl.java
@@ -1,5 +1,19 @@
 package com.example.demo.service.Impl;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.service.ClaimService;
+import com.example.demo.model.Claim;
+import com.example.demo.repository.ClaimRepository;
+@Service
 public class ClaimServiceImpl{
-    
+    @Autowired ClaimRepository claimRepository;
+    public ClaimServiceImpl(ClaimRepository claimRepository){
+        this.claimRepository=claimRepository;
+    }
+    @Override
+    public Claim createClaim(claim claim){
+        claim.setStatus("PENDING");
+        return claimRepository
+    }
 }
--- src/main/java/com/example/demo/service/Impl/PolicyServiceImpl.java
+++ src/main/java/com/example/demo/service/Impl/PolicyServiceImpl.java
@@ -3,9 +3,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.service.UserService;
-import com.example.demo.model.User;
-import com.example.demo.repository.UserRepository;
+import com.example.demo.service.PolicyService;
+import com.example.demo.model.Policy;
+import com.example.demo.repository.PolicyRepository;
 @Service
 public class PolicyServiceImpl{
     @Autowired PolicyRepository policyRepository;
